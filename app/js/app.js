5e72406a493bf827c83174d2b58150df09cc30d1
Isai Vazquez <me@isaivazquez.com>
2014-07-06 17:35:29 -0700

    Testing Facebook SDK

--- app/js/app.js
+++ app/js/app.js
@@ -1,16 +1,18 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+angular.module('ommch', [
+    'ngRoute',
+    'facebook',
+    'ommch.filters',
+    'ommch.services',
+    'ommch.directives',
+    'ommch.controllers'
+])
+.config(['$routeProvider', 'FacebookProvider', function ($routeProvider, FacebookProvider) {
+    $routeProvider.when('/', {templateUrl: 'partials/facebook.html', controller: 'FacebookTest'});
+    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+    $routeProvider.otherwise({redirectTo: '/view1'});
+
+    FacebookProvider.init('1434728606812080');
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,53 @@
-'use strict';
+(function (angular) {
+    'use strict';
 
-/* Controllers */
+    /* Controllers */
 
-angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+    angular.module('ommch.controllers', [])
+        .controller('MyCtrl1', ['$scope', function ($scope) {
+    }])
+    .controller('MyCtrl2', ['$scope', function ($scope) {
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+    }])
+    .controller('FacebookTest', ['$scope','Facebook', function ($scope, Facebook) {
+         // Here, usually you should watch for when Facebook is ready and loaded
+        $scope.$watch(function () {
+            return Facebook.isReady(); // This is for convenience, to notify if Facebook is loaded and ready to go.
+        }, function (newVal) {
+            $scope.facebookReady = true; // You might want to use this to disable/show/hide buttons and else
+        });
 
-  }]);
+        // From now on you can use the Facebook service just as Facebook api says
+        // Take into account that you will need $scope.$apply when inside a Facebook function's scope and not angular
+        $scope.login = function () {
+            Facebook.login(function (response) {
+             // Do something with response. Don't forget here you are on Facebook scope so use $scope.$apply
+            });
+        };
+
+        $scope.getLoginStatus = function () {
+            Facebook.getLoginStatus(function (response) {
+                if (response.status == 'connected') {
+                    $scope.$apply(function () {
+                        $scope.loggedIn = true;
+                    });
+                }
+                else {
+                    $scope.$apply(function () {
+                        $scope.loggedIn = false;
+                    });
+                }
+            }
+
+            $scope.me = function () {
+                Facebook.api('/me', function (response) {
+                    $scope.$apply(function () {
+                      // Here you could re-check for user status (just in case)
+                        $scope.user = response;
+                };
+            );
+          });
+        };
+    }]);
+
+}(angular));
